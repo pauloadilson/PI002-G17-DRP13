f167f9fc163bfba4ca8cbd0a01bf3f2ccc3b8a12
Paulo Adilson da Silva Frattini <pauloadilson@gmail.com>
2024-10-15 08:44:29 -0300

    Minor change. Title for login page.

--- login/views.py
+++ login/views.py
@@ -6,7 +6,13 @@ class CustomLoginView(LoginView):
     form_class = AuthenticationForm  # Utilizando o formulário padrão de autenticação
     template_name = 'login.html'  # Template que será renderizado
     redirect_authenticated_user = True
+    title = "Login"  # Título da página
     success_url = reverse_lazy('index')  # Página para onde o usuário será redirecionado após o login
 
     def get_success_url(self):
         return self.success_url
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.title
+        return context
